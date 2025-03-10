44acd445f756d43e0ae0e2e3c0553d8b61ec7969
Eduardo Frota <eduardocfrota901@gmail.com>
2025-03-09 20:51:26 -0400

    dinamizando os dados do post

--- ola-mundo/src/paginas/post/post.js
+++ ola-mundo/src/paginas/post/post.js
@@ -1,13 +1,22 @@
 import styles from "paginas/post/post.module.css"
 import { useParams } from "react-router-dom"
+import posts from "json/posts.json"
+import PostModelo from "componentes/post-modelo/post-modelo"
 function Post(){
     const paramentros = useParams()
-    console.log(paramentros);
+
+    const post = posts.find((post) => {
+        return post.id === Number(paramentros.id)
+    })
     
+    const capas = require(`assets/posts/${post.id}/capa.png`)
+
+    console.log(post);
     return (
-        <h1>
-            Post: {paramentros.id}
-        </h1>
+        <PostModelo fotoCapa={capas} titulo={post.titulo}>
+
+            Texto...
+        </PostModelo>
     )
 }
 export default Post
