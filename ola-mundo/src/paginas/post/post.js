0ff9ff4bd725c6170f96a3276aa6f9cc455bcd5c
Eduardo Frota <eduardocfrota901@gmail.com>
2025-03-16 18:22:43 -0400

    Tratando pagina de post

--- ola-mundo/src/paginas/post/post.js
+++ ola-mundo/src/paginas/post/post.js
@@ -1,27 +1,33 @@
 import "paginas/post/post.css"
-import { useParams } from "react-router-dom"
+import { Route, Routes, useParams } from "react-router-dom"
 import posts from "json/posts.json"
 import PostModelo from "componentes/post-modelo/post-modelo"
 import ReactMarkdown from "react-markdown"
+import PaginaPadrao from "componentes/pagina-padrao/paginaPadrao"
 
-function Post(){
+function Post() {
     const paramentros = useParams()
 
     const post = posts.find((post) => {
         return post.id === Number(paramentros.id)
     })
-    
+
     const capas = require(`/public/imagens/${post.id}/capa.png`)
 
     console.log(post);
     return (
-        <PostModelo fotoCapa={capas} titulo={post.titulo}>
-            <div className="post-markdown-container">
-                <ReactMarkdown>
-                    {post.texto}
-                </ReactMarkdown>
-            </div>
-        </PostModelo>
+        <Routes>
+            <Route path="*" element={<PaginaPadrao />}>
+                <Route index element={
+                    <PostModelo fotoCapa={capas} titulo={post.titulo}>
+                        <div className="post-markdown-container">
+                            <ReactMarkdown>
+                                {post.texto}
+                            </ReactMarkdown>
+                        </div>
+                    </PostModelo>} />
+            </Route>
+        </Routes>
     )
 }
 export default Post
